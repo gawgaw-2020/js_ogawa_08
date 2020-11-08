b5636808512e7843c578f20d97d450cbdfc0792f
gawgaw <gawgaw.developer@gmail.com>
2020-11-08 11:57:16 +0900

    スタッフの参照機能追加

---
+++ assets/js/staffDispMain.js
@@ -0,0 +1,39 @@
+// ストレージにデータがなければログイン画面へ遷移
+if (storage.teacher_id === undefined || storage.teacher_name === undefined) {
+  window.location.href = '/js_ogawa_08/staff-login/staff_login.html';
+}
+
+// ------------------要素の取得------------------
+const teacher_id = localStorage.getItem('teacher_id');
+const teacher_name = localStorage.getItem('teacher_name');
+const selected_teacher_id = localStorage.getItem('selected_teacher_id');
+const loginName = document.getElementById('js-login-name');
+
+const  selectedTeacherId = document.getElementById('js-selected_teacher_id');
+const  selectedTeacherName = document.getElementById('js-selected_teacher_name');
+
+// --------------要素の取得ここまで--------------
+
+let selected_teacher_name;
+
+// ログイン中の講師の名前を表示
+loginName.textContent = teacher_name + '先生ログイン中';
+
+// 講師コードを表示
+selectedTeacherId.textContent = selected_teacher_id;
+
+// IDからドキュメントを取得、名前を表示
+var docRef = db.collection("teachers").doc(selected_teacher_id);
+docRef.get().then(function(doc) {
+    if (doc.exists) {
+        console.log("Document data:", doc.data());
+        selected_teacher_name = doc.data().teacher_name;
+        selectedTeacherName.textContent = selected_teacher_name;
+        
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+}).catch(function(error) {
+    console.log("Error getting document:", error);
+});
--- assets/js/staffListMain.js
+++ assets/js/staffListMain.js
@@ -14,7 +14,6 @@ const teacherEdit = document.getElementById('js-teacher-edit');
 const teacherDelete = document.getElementById('js-teacher-delete');
 // --------------要素の取得ここまで--------------
 
-
 // ログイン中の講師の名前を表示
 loginName.textContent = teacher_name + '先生ログイン中';
 
@@ -33,14 +32,38 @@ db.collection("teachers").get().then(function(querySnapshot) {
 teacherAdd.addEventListener('click', () => {
   window.location.href = '/js_ogawa_08/staff/staff_add.html';
 });
+
+
 // 参照ボタン押した処理
 teacherDisp.addEventListener('click', () => {
+    // 要素を取得
+  var elements = document.getElementsByName( "staffcode" ) ;
 
+  // 選択状態の値を取得
+  for ( var selected_teacher_id = "", i = elements.length; i--; ) {
+    if ( elements[i].checked ) {
+      var selected_teacher_id = elements[i].value ;
+      break ;
+    }
+  }
+
+  if ( selected_teacher_id === "" ) {
+    // 何も選択されていなかった場合
+    window.location.href = '/js_ogawa_08/staff/staff_ng.html';
+  } else {
+    // 選択状態の値が代入されている場合
+    localStorage.setItem('selected_teacher_id', selected_teacher_id);
+    window.location.href = '/js_ogawa_08/staff/staff_disp.html';
+  }
 });
+
+
 // 修正ボタン押した処理
 teacherEdit.addEventListener('click', () => {
 
 });
+
+
 // 削除ボタン押した処理
 teacherDelete.addEventListener('click', () => {
   // 要素を取得
